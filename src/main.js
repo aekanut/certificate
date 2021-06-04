24d0ab8bec0b7274b4e2eb9733a8bf3c1c3be4ca
ball <aekanut.rungseevuttana@gmail.com>
2021-06-04 16:47:45 +0700

    try bootstrap

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import store from "./store"
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-
 createApp(App)
   .use(store)
   .use(router)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,42 @@
-<template lang="">
-  <div>
-      
+<template>
+  <div class="center w-50">
+    <div class="container border border-3 rounded p-5" style="width:500px;">
+      <form>
+        <h1 class="mb-5 fw-normal text-center fw-bold">Sign in</h1>
+        <div class="form-floating mt-3">
+          <input
+            type="email"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating mt-3">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">
+          Sign in
+        </button>
+      </form>
+    </div>
   </div>
 </template>
 <script>
 export default {}
 </script>
-<style lang=""></style>
+<style scoped>
+.center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
